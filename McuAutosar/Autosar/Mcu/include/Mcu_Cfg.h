72cc7d625e63da915724d8bfcbf5abe80195216e
hienvt123pro <hiengymer1018@gmail.com>
2023-11-08 09:26:28 +0700

    push mode and ram

--- McuAutosar/Autosar/Mcu/include/Mcu_Cfg.h
+++ McuAutosar/Autosar/Mcu/include/Mcu_Cfg.h
@@ -61,6 +61,45 @@ extern "C"{
                                  STRUCTURES AND OTHER TYPEDEFS
 ==================================================================================================*/
 
+/**
+* @brief            The Mcu_ModeType specifies the identification (ID) for a MCU mode, configured via configuration structure.
+* @details          The type shall be uint8, uint16 or uint32.
+*
+* @implements     Mcu_ModeType_typedef
+*
+*/
+typedef uint32 Mcu_ModeType;
+
+/**
+* @brief            The Mcu_RamSectionType specifies the identification (ID) for a RAM section, configured via the configuration structure.
+*                   The type shall be uint8, uint16 or uint32, based on best performance.
+* @implements Mcu_RamSectionType_typedef
+*/
+typedef uint32 Mcu_RamSectionType;
+
+/**
+* @brief            The Mcu_RamSizeType specifies the RAM section size.
+*                   The type shall be uint8, uint16 or uint32, based on best performance.
+*
+*/
+typedef uint32 Mcu_RamSizeType;
+
+/**
+* @brief            The Mcu_RamWriteSizeType specifies the RAM section write size.
+*                   The type shall be uint8, uint16 or uint32, based on best performance.
+*
+*/
+typedef uint32 Mcu_RamWriteSizeType;
+
+#if (MCU_INIT_CLOCK == STD_ON)
+/**
+* @brief          Defines the identification (ID) for clock setting configured via the configuration structure.
+* @details        The type shall be uint8, uint16 or uint32, depending on uC platform.
+*
+* @implements Mcu_ClockType_typedef
+*/
+typedef uint32 Mcu_ClockType;
+#endif
 
 /*==================================================================================================
                                  GLOBAL VARIABLE DECLARATIONS
